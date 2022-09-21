a96a7ef588890c472ee26f08d6a933dd8651685e
mohamadalghanem474 <mohamad030_2@hotmail.com>
2022-09-21 23:53:33 +0300

    upload file image in note

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
 
+define("MB",1048576);
 
 function filterRquest($RequsetName){
 
@@ -7,7 +8,27 @@ function filterRquest($RequsetName){
 
  }
 
+ function uploadImage($imageRequest){
+     global $msgError;
+    $imageName     = $_FILES[$imageRequest]["name"] . rand(1,99999);
+    $imageTmpName  = $_FILES[$imageRequest]["tmp_name"];
+    $imageSize     = $_FILES[$imageRequest]["size"];
+    $alloweExt     = array("jpg","jpge","png","mp3","pdf","txt");
+    $stringToArray = explode(".",$imageName);
+    $Ext = end($stringToArray);
+    $Ext = strtolower($Ext);
 
-
-
-
+    if (!empty($imageName&&!in_array($Ext,$alloweExt))) {
+        $msgError[] ="Ext";
+    }
+    if ($imageSize > 2*MB) {
+        $msgError[] ="Size";
+    }
+    if (empty($msgError)) {
+      move_uploaded_file($imageTmpName,"../upload/".$imageName);
+      return $imageName;
+    }
+    else{
+        return "error";
+    }
+ }
--- notes/add.php
+++ notes/add.php
@@ -4,16 +4,16 @@ include "../connect.php";
 $title = filterRquest("title");
 $content = filterRquest("content");
 $userid = filterRquest("userid");
+$imageName = uploadImage("file");
 
-
-
-$stmt = $con->prepare(
+if ($imageName!=="error") {
+    $stmt = $con->prepare(
     "INSERT
-     INTO `notes`(`note_title`, `note_content`, `note_user_id`)
-     VALUES (?,?,?)"
+     INTO `notes`(`note_title`, `note_content`, `note_user_id`,`note_image`)
+     VALUES (?,?,?,?)"
 );
 
-$stmt->execute(array($title,$content,$userid));
+$stmt->execute(array($title,$content,$userid,$imageName));
 $count = $stmt->rowCount();
 
 if ($count>0) {
@@ -21,3 +21,10 @@ if ($count>0) {
 } else {
     echo json_encode(array("status"=>"error"));
 }
+}else{
+    echo json_encode(array("status"=>"error"));
+}
+
+
+
+
